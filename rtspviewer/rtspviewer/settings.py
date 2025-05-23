a15d47da17a239552040a80ad34475c2a0f54b77
Ayreen <ayreenJ.022@gmail.com>
2025-05-23 20:45:43 +0530

    Fix: Minor fix

--- rtspviewer/rtspviewer/settings.py
+++ rtspviewer/rtspviewer/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://rtsp-stream-django-react.vercel.app/', # Vercel deployment
+    'https://rtsp-stream-django-react.vercel.app', # Vercel deployment
     "http://localhost:5173",  # React development server
     "http://127.0.0.1:3000",
     # Add your production domains if applicable
